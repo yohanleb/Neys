d4f741d958f95717d1945a40d53b1282b290f970
yohanleb <yohan.lebricquer@gmail.com>
2019-11-19 23:01:53 +0100

    Detailed view of an article

--- app/src/main/java/com/yohan/neys/model/Article.java
+++ app/src/main/java/com/yohan/neys/model/Article.java
@@ -1,12 +1,30 @@
 package com.yohan.neys.model;
 
-public class Article {
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class Article implements Serializable {
+    private Source source;
     private String author;
     private String title;
     private String description;
+    private String url;
     private String urlToImage;
     private String publishedAt;
     private String content;
+
+    public Source getSource() {
+        return source;
+    }
 
     public String getAuthor() {
         return author;
@@ -20,12 +38,27 @@ public class Article {
         return description;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
     public String getUrlToImage() {
         return urlToImage;
     }
 
     public String getPublishedAt() {
         return publishedAt;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String getPublishedAtFormatted() {
+        Instant instant = Instant.parse(publishedAt);
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern( "'Le' E dd/MM 'à' HH:mm" )
+                        .withLocale( Locale.FRANCE )
+                        .withZone(ZoneId.systemDefault());
+
+        return formatter.format(instant);
     }
 
     public String getContent() {
---
+++ app/src/main/java/com/yohan/neys/model/Source.java
@@ -0,0 +1,16 @@
+package com.yohan.neys.model;
+
+import java.io.Serializable;
+
+public class Source implements Serializable {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
